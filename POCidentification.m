fbc80d628f207ab99271e3263938a59cd808feed
Kurt Dudek <kpdudek@bu.edu>
2018-06-05 17:10:20 -0400

    Added ct vs time plotting

--- POCidentification.m
+++ POCidentification.m
@@ -1,14 +1,14 @@
 function POCidentification
 %Loads output of POC_tracks_alignment
-load('POC_tracks_alignment_data_2018_06_05_4Corners_Manual.mat')
+load('POC_tracks_alignment_data_2018_06_05_4Corners_Acro.mat')
 
 %Takes the data input, and forms the matricies used in future calculations
 [omega,T] = create_matricies(a_o1,a_o2,a_o3,a_o4,a_fz,a_tx,a_ty,a_tz);
 
 %Span to calculate coefficients over
-%load('POCidentification_test_span_2018_06_05_4Corners_Manual.mat')
-n1 = [679,6309,10380,13000,15320,18310];
-n2 = [6281,9902,12380,14890,17410,27520];
+load('POCidentification_test_span_2018_06_05_4Corners_Acro.mat')
+% n1 = [679,6309,10380,13000,15320,18310];
+% n2 = [6281,9902,12380,14890,17410,27520];
 len_n1 = length(n1);
 len_n2 = length(n2);
 
@@ -54,10 +54,12 @@ print_stars()
 %average_independent_coefficients(omega,T,n1,n2,len_n1)
 
 
+[indep_discr_coef,discreet_coef] = discreet_coefs(omega,T,n1,n2);
+
 
 %savefig(figures,'Figures_POC_tracks_alignment_data_2018_06_05_4Corners_Acro.fig')
-save([mfilename '_all_coefs_2018_06_05_4Corners_Manual.mat'],'coef','coef_ave','independent_coef','omega','T')
-save([mfilename '_test_span_2018_06_05_4Corners_Manual.mat'],'n1','n2')
+save([mfilename '_all_coefs_2018_06_05_4Corners_Acro.mat'],'coef','coef_ave','independent_coef','discreet_coef','indep_discr_coef','omega','T')
+%save([mfilename '_test_span_2018_06_05_4Corners_Acro.mat'],'n1','n2')
 
 
 
@@ -418,6 +420,33 @@ time = 1:length(T_plot(1,:));
 
 mat = plot(mat_ax,time,T_plot(1,:),'r:',time,T_plot(2,:),'k:',time,T_plot(3,:),'g:',time,T_plot(4,:),'b:',time,a_fz,'r',time,a_tx,'k',time,a_ty,'g',time,a_tz,'b');
 legend(mat,'Calculated Fz','Calculated Tx','Calculated Ty','Calculated Tz','Force Z','Torque X','Torque Y','Torque Z','Orientation','horizontal')
+
+
+
+%Calculates the coefficients at each data instance
+function [indep_discr_coef,discreet_coef] = discreet_coefs(omega,T,n1,n2)
+discreet_coef = [];
+for i = n1(1):n2(end)
+    osq = omega(:,i).^2;
+    Ti = T(:,i);
+    mat_o = [sum(osq),0,0;0,osq(1)-osq(2)+osq(3)-osq(4),0;0,-osq(1)+osq(2)+osq(3)-osq(4),0;0,0,-osq(1)+osq(2)-osq(3)+osq(4)];
+    coefs = mat_o\Ti;
+    discreet_coef = [discreet_coef,coefs];
+end
+
+
+indep_discr_coef = [];
+% for j = 1:length(omega)
+%     osq = omega(:,j).^2;
+%     i_Ti = T(:,j);
+%     i_mat_o = [osq(1),osq(2),osq(3),osq(4),0,0,0,0,0,0,0,0;
+%         0,0,0,0,osq(1),-osq(2),osq(3),-osq(4),0,0,0,0;
+%         0,0,0,0,-osq(1),osq(2),osq(3),-osq(4),0,0,0,0;
+%         0,0,0,0,0,0,0,0,-osq(1),osq(2),-osq(3),osq(4)];
+%     i_coefs = i_mat_o\i_Ti;
+%     
+%     indep_discr_coef = [indep_discr_coef,i_coefs];
+% end
 
 %Prints a line of asteriscs for output separation
 function print_stars()
--- lsnonlintest.m
+++ lsnonlintest.m
@@ -1,6 +1,6 @@
 function lsnonlintest
-load('POCidentification_all_coefs_2018_06_05_4Corners_Manual.mat')
-load('POCidentification_test_span_2018_06_05_4Corners_Manual.mat')
+load('POCidentification_all_coefs_2018_06_05_4Corners_Acro.mat')
+load('POCidentification_test_span_2018_06_05_4Corners_Acro.mat')
 
 fitted_coefs = [];
 
@@ -21,6 +21,9 @@ plot_coef_vs_fitted(coef,fitted_coefs)
 use_LS_values(fitted_coefs,omega,T)
 
 ct_vs_omega(omega,coef,n1,n2)
+
+plot_ct_all_omega(indep_discr_coef,discreet_coef,omega,n1,n2)
+
 
 
 function f = residuals(FT_true,omega,x)
@@ -98,16 +101,40 @@ fun2 = @(x,data) x(1)./(1+(1./(data-x(2)).^x(3)));
 fit2 = lsqcurvefit(fun2,x1,ave_omega,coef(1,:));
 fprintf('beta = %e\ngamma = %e\npower = %e\n',fit2(1),fit2(2),fit2(3))
 
-plot(ave_omega,coef(1,:),ave_omega,fit(1)*log(ave_omega-fit(2)),ave_omega,fit2(1)./(1+(1./(ave_omega-fit2(2)).^fit2(3))),ave_omega,(2.089*10^-13).*ave_omega.^2.179,ave_omega,(2.2233514*10^-6)./(1+10245.12196*exp(-.0072175656.*ave_omega)))
-legend('cT','log Fitted cT','exponential Fitted','Power Fit','Log Fit')
+x2 = [(2.089*10^-6),10245,.007217];
+fun3 = @(x,data) x(1)./(1+x(2)*exp(-x(3).*ave_omega));
+fit3 = lsqcurvefit(fun3,x2,ave_omega,coef(1,:));
+fprintf('beta = %e\ngamma = %e\npower = %e\n',fit3(1),fit3(2),fit3(3))
 
+plot(ave_omega,coef(1,:),ave_omega,fit(1)*log(ave_omega-fit(2)),ave_omega,fit2(1)./(1+(1./(ave_omega-fit2(2)).^fit2(3))),ave_omega,(fit3(1))./(1+fit3(2)*exp(-fit3(3).*ave_omega)))
+legend('cT','log Fitted cT','exponential Fitted','e Fitted')
 
+function plot_ct_all_omega(indep_discr_coef,discreet_coef,omega,n1,n2)
+figure('Visible','on')
+xlabel('Omega')
+ylabel('ct')
 
+omega_ave = mean(omega(:,n1(1):n2(end)));
 
+ct = uitab('Title','ct vs omega');
+ct_ax = axes(ct);
+plot(ct_ax,1:length(omega_ave),discreet_coef(1,:)) %,1:length(omega_ave),condition(omega_ave),
 
+dt = uitab('Title','d vs omega');
+d_ax = axes(dt);
+d = discreet_coef(2,:)./discreet_coef(1,:);
+plot(d_ax,omega_ave,d)
 
+% i_ct = uitab('Title','cts vs omega');
+% i_ct_ax = axes(i_ct);
+% plot(i_ct_ax,omega(1,:),indep_discr_coef(1,:),omega(2,:),indep_discr_coef(2,:),omega(3,:),indep_discr_coef(3,:),omega(4,:),indep_discr_coef(4,:))
+
+function s_conditioned=condition(s)
+s=shiftdim(s);
+sMax=max(s);
+sMin=min(s);
+s_conditioned=(s-sMin)/(sMax-sMin);
 
 % for i = 1:length(omega)
 %     ft = [ft,(coef_mat*omega(:,i))];
 % end
-
